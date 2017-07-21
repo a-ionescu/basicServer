63a65095a9039daf46cb5a63df4e84bafa5c8e4c
a-ionescu <aionescu@luxoft.com>
2017-07-21 17:35:26 +0300

    update

--- app/src/main/java/com/spirent/pttb/basicserver/MainActivity.java
+++ app/src/main/java/com/spirent/pttb/basicserver/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.nfc.Tag;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,21 +21,21 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final int portNumber = 6000;
-    private static final int accessP = 0;
+    private ServerSocket serverSocket;
+    private static String nmeaData = "null";
+    private boolean recordNmea = false;
+    private boolean recordData = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //int permissionCheck = ContextCompat.checkSelfPermission(this,
-        //       Manifest.permission.ACCESS_FINE_LOCATION);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -42,13 +43,7 @@ public class MainActivity extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    accessP);
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    accessP);
-
+            return;
         }
 
 
@@ -75,38 +70,70 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+
         LocationManager LM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        LM.addNmeaListener(new GpsStatus.NmeaListener() {
-            public void onNmeaReceived(long timestamp, String  nmea) {
-                Log.d("NMEA", ":" + nmea);
-            }
-        });
-
-
-
-
-        ServerSocket serverSocket;
         try {
-            Toast.makeText(this,"createServer",Toast.LENGTH_LONG).show();
-            serverSocket = new ServerSocket(portNumber);
-            Socket socket = serverSocket.accept();
-            Toast.makeText(this,"Accept",Toast.LENGTH_LONG).show();
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            LM.addNmeaListener(new GpsStatus.NmeaListener()  {
 
-            bw.write("Message from the server");
-            Toast.makeText(this,"Message from the server",Toast.LENGTH_LONG).show();
-            //Insert new line \n
-            bw.newLine();
-            //Send the message
-            bw.flush();
+                public void onNmeaReceived(long timestamp, String nmea){
+                        //check nmea's checksum
 
-            //Receive a message from client
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            //System.out.println("Message from the client: " + br.readLine());
-            //System.out.println("Server has ended");
+                        //text.setText(nmea);
+                        recordNmea = true;
+                        nmeaData = nmea;
+                        recordNmea = false;
+                        Log.d("GPS-NMEA", nmea);
 
-        } catch (IOException e) {
+
+
+                }
+            });
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+        new Thread() {
+
+            @Override
+                    public void run() {
+                try {
+                    //Toast.makeText(this, "createServer", Toast.LENGTH_LONG).show();
+                    Log.i("Server", "Attempt to create server");
+                    serverSocket = new ServerSocket(portNumber);
+                    Log.i("Server", "serverSocket = new ServerSocket(portNumber);");
+                    //Toast.makeText(this, "serverSocket = new ServerSocket(portNumber);", Toast.LENGTH_LONG).show();
+                    Socket socket = serverSocket.accept();
+                    Log.i("Server", "Socket socket = serverSocket.accept();");
+                    //Toast.makeText(this, "Accept", Toast.LENGTH_LONG).show();
+                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+                    while (recordData = true) {
+                        if (recordNmea = true) {
+                            //bw.write("Message from the server");
+                            bw.write(nmeaData);
+                            //Toast.makeText(this, "Message from the server", Toast.LENGTH_LONG).show();
+                            //Insert new line \n
+                            bw.newLine();
+                            //Send the message
+                            bw.flush();
+                        }
+                        break;
+                    }
+                    //Receive a message from client
+                    //BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    //System.out.println("Message from the client: " + br.readLine());
+                    //System.out.println("Server has ended");
+
+                } catch (
+                        IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
+
+
+
+
 }
